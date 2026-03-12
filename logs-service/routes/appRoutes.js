70af503df2387c0b63ff288caa5938787561ae54
shubham <shubhamonline024@gmail.com>
2026-03-12 15:03:41 +0530

    removed logs-service not required

--- logs-service/routes/appRoutes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { handleLogs } = require("../controllers/controllers");
-
-router.post("/logs", handleLogs);
-// router.post("/status", jobStatus);
-
-module.exports = router;
